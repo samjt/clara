59e709d76e69aff0b0ab003e197ff399e9e0247c
Sam Thorne <sam.thorne@me.com>
2022-06-29 16:49:34 +0200

    Pin YA_FSM to 1.0.5
    Add basic states setup
    Pin version to latest not old 1.1.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,49 @@
 #include <Arduino.h>
 #include <YA_FSM.h>
+
 YA_FSM myState;
 
-
-enum State {BLINK1, BLINK2, BLINK3};
+enum State
+{
+	DefaultPattern,
+	BlinkRed,
+	BlinkGreen,
+	BlinkBlue,
+	BlinkYellow
+};
 
 // Helper for print labels instead integer when state change
-const char * const stateName[] PROGMEM = { "Blink1", "Blink2", "Blink3"};
+const char *const stateName[] PROGMEM = {"DefaultPattern", "BlinkRed", "BlinkGreen", "BlinkBlue", "BlinkYellow"};
 
-
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+	// put your setup code here, to run once:
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+	// put your main code here, to run repeatedly:
 }
 
-void setupStateMachine(){
-  
-  // Follow the order of defined enumeration for the state definition (will be used as index)
-	//Add States  => name, 		timeout, onEnter callback, onState cb, 	  onLeave cb	
-	myState.AddState(stateName[BLINK1], 0, onEntering, onStateBlink1, onLeaving);
-	myState.AddState(stateName[BLINK2], 0, onEntering, onStateBlink2, onLeaving);
-	myState.AddState(stateName[BLINK3], 0, onEntering, onStateBlink3, onLeaving);
+void onStateBlink()
+{
+	myState.ActiveStateName();
+}
 
-	// Add transitions with related "trigger" callback functions	
-	myState.AddTransition(BLINK1, BLINK2, checkButton );
-	myState.AddTransition(BLINK2, BLINK3, checkButton );
+void setupStateMachine()
+{
+
+	// Follow the order of defined enumeration for the state definition (will be used as index)
+	// Add States  => name, 		timeout, onEnter callback, onState cb, 	  onLeave cb
+	myState.AddState(stateName[BlinkRed], 0, onEntering, onStateBlink, onLeaving);
+	myState.AddState(stateName[BlinkGreen], 0, onEntering, onStateBlink, onLeaving);
+	myState.AddState(stateName[BlinkBlue], 0, onEntering, onStateBlink, onLeaving);
+	myState.AddState(stateName[BlinkYellow], 0, onEntering, onStateBlink, onLeaving);
+
+	// Add transitions with related "trigger" callback functions
+	myState.AddTransition(BlinkRed, BlinkGreen, checkButton);
+	myState.AddTransition(BlinkGreen, BlinkBlue, checkButton);
 	// For this transition let'use a bool variable (must be global, because we need fix address)
-	myState.AddTransition(BLINK3, BLINK1, dummyBoolVar );
+	myState.AddTransition(BlinkBlue, BlinkRed, dummyBoolVar);
+	myState.AddTransition(BlinkBlue, BlinkRed, dummyBoolVar);
 }
