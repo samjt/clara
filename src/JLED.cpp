6d016429c26ba4a9985773bb6db06c376398f670
Sam Thorne <sam.thorne@me.com>
2022-07-07 08:55:29 +0200

    LED libs

---
+++ src/JLED.cpp
@@ -0,0 +1,54 @@
+// JLed custom HAL example.
+// Copyright 2019 by Jan Delgado. All rights reserved.
+// https://github.com/jandelgado/jled
+
+// we include jled_base.h instead of "jled.h" since we define our own JLed
+// class using our custom HAL.
+#include <jled_base.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+// a custom HAL for the Arduino, inverting output and ticking with half
+// the speed. In general, a JLed HAL class must satisfy the following
+// interface:
+//
+// class JledHal {
+//   public:
+//     JledHal(PinType pin);
+//     void analogWrite(uint8_t val) const;
+//     uint32_t millis() const;
+//  }
+//
+class CustomHal
+{
+public:
+    using PinType = uint8_t;
+
+    explicit CustomHal(PinType pin) noexcept : pin_(pin)
+    {
+    }
+
+    void analogWrite(uint8_t val) const
+    {
+        // some platforms, e.g. STM need lazy initialization
+        if (!setup_)
+        {
+            pwm.begin();
+            pwm.setPWMFreq(1600); // This is the maximum PWM frequency
+            Wire.setClock(400000);
+            setup_ = true;
+        }
+        pwm.setPWM(pin_, 0, val);
+    }
+
+    uint32_t millis() const { return ::millis(); }
+
+private:
+    Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+    mutable bool setup_ = false;
+    PinType pin_;
+};
+
+class JLed : public jled::TJLed<CustomHal, JLed>
+{
+    using jled::TJLed<CustomHal, JLed>::TJLed;
+};
